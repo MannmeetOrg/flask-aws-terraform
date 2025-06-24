f14511f19ac2babfee93567b3f5644af9d920359
MannuA19 <mannmeet.ahir@gmail.com>
2025-06-24 22:01:01 +0530

    Terraform Code Updated

--- Deployment/backend.tf
+++ Deployment/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "flaskapp-bucket"
+    bucket         = "flask-app-s3bucket"
     key            = "flaskapp/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
