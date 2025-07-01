ae3654ebc170c1737ed586fcfec4f218a5184f76
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-01 20:48:05 +0530

    Terraform Code Updated

--- Deployment/key-pair.tf
+++ Deployment/key-pair.tf
@@ -1,5 +1,5 @@
 # Create a Key Pair resource in AWS
 resource "aws_key_pair" "my_key" {
   key_name   = var.key_pair_name
-  public_key = var.public_key
+  public_key = file("/c/Users/manmeet/.ssh/key-pair.pub")
 }
