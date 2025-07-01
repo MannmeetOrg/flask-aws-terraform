505ea7c2600362d46879f5f123d394ce21798545
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-01 21:53:52 +0530

    Terraform Code Updated

--- Deployment/vpc.tf
+++ Deployment/vpc.tf
@@ -3,14 +3,6 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_cidr_block
-  availability_zone       = var.subnet_az
-  map_public_ip_on_launch = true
-}
-
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
